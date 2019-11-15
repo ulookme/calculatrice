c3d3acc3e47107980f3cc5d83bb5ba590be58146
Charles Hajjar <hajjar.charles@icloud.com>
2019-11-15 04:05:13 +0100

    modif

--- calculatrice/main.swift
+++ calculatrice/main.swift
@@ -8,5 +8,72 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+func input() -> Int {
+    let strData = readLine()
+    
+    return Int(strData!)!
+}
+
+
+var nombre1 : Int
+var nombre2 : Int
+var operateur : Int
+
+var resultat: Int
+
+repeat {
+    print("---Menu---")
+    print("1. --Addition--")
+    print("2. --Sustraction--")
+    print("3. --Multiplication--")
+    print("4. --Division--")
+    print("5.--Modulo--")
+    
+    print(" Le choix de votre menu ? ")
+    
+    operateur = input()
+    print("")
+    
+    if (operateur > 0 && operateur < 6) {
+        print(" Entrée le premier nombre :")
+        nombre1 = input()
+        
+        print("entrer le deuxieme nombre : ")
+        nombre2 = input()
+        
+        switch operateur {
+        case 1:
+            resultat = nombre1 + nombre2
+            print("votre resultate est egale a : \(resultat)")
+            
+        case 2:
+            resultat = nombre1 - nombre2
+            print("votre resultate est egale a : \(resultat)")
+        case 3:
+            resultat = nombre1 * nombre2
+            print("votre resultate est egale a : \(resultat)")
+        case 4:
+            resultat = nombre1 / nombre2
+            print("votre resultate est egale a : \(resultat)")
+        case 5:
+            resultat = nombre1 % nombre2
+            print("votre resultate est egale a : \(resultat)")
+        default:
+            resultat = 0
+            
+            print("je ne pas saisi votre demande une erreur de calcule est survenue")
+            
+            print("")
+            print(" votre resultat est de : \(resultat)")
+            print("")
+            
+        }
+        
+        
+    }
+    
+    
+    
+}while operateur < 1 || operateur > 5
